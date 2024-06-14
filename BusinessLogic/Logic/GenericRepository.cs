7195febccc20c8f41141c668f1634a9e27c313af
Daniel Salazar <dsnile.salazr@gmail.com>
2024-06-14 14:20:57 -0600

    feat: Add GenericRepository class for generic data access

---
+++ BusinessLogic/Logic/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+using System.Security.Cryptography.X509Certificates;
+using BusinessLogic.Data;
+
+namespace BusinessLogic.Logic;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : ClaseBase
+{
+    private readonly TaskDbContext _context;
+
+    public GenericRepository(TaskDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> add(T entity)
+    {
+        _context.Set<T>().Add(entity);
+
+        return await _context.SaveChangesAsync();
+    }
+
+    public void AddEntity(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
+
+    public void DeleteEntity(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
+
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task<int> update(T entity)
+    {
+        _context.Set<T>().Attach(entity);
+        return await _context.SaveChangesAsync();
+    }
+
+    public void updateEntity(T entity)
+    {
+        _context.Set<T>().Attach(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+    }
+}
---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces;
+
+public interface IGenericRepository<T> where T : ClaseBase
+{
+    Task<T> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<int> add(T entity);
+
+    Task<int> update(T entity);
+
+    void AddEntity(T entity);
+
+    void updateEntity(T entity);
+    void DeleteEntity(T entity);
+
+
+}
