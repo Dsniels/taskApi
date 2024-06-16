04738f7314773437f26daa6d8ad877dc25983c04
Daniel Salazar <dsnile.salazr@gmail.com>
2024-06-15 22:53:41 -0600

    feat: Add Tasks and Usuario entities
    The code changes include the addition of two new entity classes, Tasks and Usuario, in the Core.Entities namespace. The Tasks class has properties for Titulo, Tarea, Date, and Usuario, while the Usuario class has properties for Name, Apellido, and Tareas. These entities are derived from the ClaseBase class.
    
    Based on the recent user commits and repository commits, it seems that these entities are being added as part of a larger feature or functionality related to tasks and data persistence.

---
+++ Core/Entities/Tasks.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities;
+
+public class Tasks : ClaseBase
+{
+    
+    public string Titulo {get; set;}
+
+    public string Tarea {get; set; }
+
+    public DateTimeOffset Date {get; set; }
+
+    public Usuario Usuario { get; set;}
+
+
+}
---
+++ Core/Entities/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities;
+
+public class Usuario : ClaseBase
+{
+    public string Name { get; set; }
+    public string Apellido { get; set;}
+
+    public List<Tasks> Tareas {get; set; }
+
+
+}
