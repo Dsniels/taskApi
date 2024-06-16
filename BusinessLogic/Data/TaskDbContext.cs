f40878eaf4a9121b61790bfcbd517cab4789e44c
Daniel Salazar <dsnile.salazr@gmail.com>
2024-06-15 22:53:00 -0600

    feat: Add TaskDbContext for tasks data persistence

---
+++ BusinessLogic/Data/TaskDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BusinessLogic.Data;
+
+public class TaskDbContext : DbContext
+{
+    public TaskDbContext(DbContextOptions<TaskDbContext> options) : base( options ){}
+
+    public DbSet<Tasks> Tasks {get; set;}
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+}
