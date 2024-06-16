1fb892d8de56040728885d747b18f54837662c4f
Daniel Salazar <dsnile.salazr@gmail.com>
2024-06-15 22:52:52 -0600

    feat: Add TareasRepository class for tasks data access

---
+++ BusinessLogic/Logic/TareasRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+using BusinessLogic.Data;
+
+namespace BusinessLogic;
+
+public class TareasRepository : ITareasRepository
+{
+    private readonly TaskDbContext _context;
+
+    public TareasRepository(TaskDbContext context){
+        _context = context;
+    }
+
+
+
+    public async Task<IReadOnlyList<Tasks>> GetAllAsync()
+    {
+        return await _context.Tasks.ToListAsync();
+    }
+
+    public async Task<Tasks> GetTaskAsync(int id)
+    {
+        return await _context.Tasks.FirstOrDefaultAsync(t => t.Id == id);
+    }
+}
