a805f694b31a79b28b4dedc5a9172e62df2484de
Daniel Salazar <dsnile.salazr@gmail.com>
2024-06-26 22:53:08 -0600

    Add methods with spec

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using Core.Entities;
+using Core.Specifications;
 
 namespace Core.Interfaces;
 
@@ -14,9 +15,10 @@ public interface IGenericRepository<T> where T : ClaseBase
     Task<int> add(T entity);
 
     Task<int> update(T entity);
-
+    Task<IReadOnlyList<T>> GetAllWithSpec(ISpecifications<T> spec);
+    Task<int> CountAsync(ISpecifications<T> spec);
+    Task<T> GetByIdWithSpec(ISpecifications<T> spec);
     void AddEntity(T entity);
-
     void updateEntity(T entity);
     void DeleteEntity(T entity);
 
